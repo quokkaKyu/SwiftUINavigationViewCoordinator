e2ee3298105997a29468d9fbef984a60ddb3d247
kyuminlee <tidls93@naver.com>
2024-04-30 12:00:11 +0900

    Initial Commit

---
+++ SwiftUINavigationViewCoordinator/SwiftUINavigationViewCoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationViewCoordinatorApp.swift
+//  SwiftUINavigationViewCoordinator
+//
+//  Created by kyuminlee on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationViewCoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
